740ae8db693cf96fd3dc8928ea98075c2367a3a9
Glo <glo.movil@gmail.com>
2023-10-12 00:54:03 +0200

    first page with 20 characters fetched - responsive

---
+++ components/Character.jsx
@@ -0,0 +1,17 @@
+
+
+const Character = ({ idCh, nameCharacter, speciesCh, originCh, imageCh }) => {
+
+    return (
+        <div className="m-5 pb-12 sm:pb-9 bg-emerald-600 flex flex-col items-center justify-center text-center w-7/12 sm:w-56 border-4 border-lime-300 rounded-lg">
+
+            <img src={imageCh} alt={nameCharacter} className="rounded-full w-8/12 -mt-7 mx-auto" />
+
+            <div className="mt-4">{nameCharacter}</div>
+            <div>{speciesCh}</div>
+            <div>{originCh}</div>
+        </div>
+    )
+}
+
+export default Character;
